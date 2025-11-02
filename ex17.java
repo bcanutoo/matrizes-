714920b0981508dc0b84c3db6a58165da1e609a7
beatriz <108158611+bcanutoo@users.noreply.github.com>
2025-11-02 12:32:44 -0300

    transformar em triangular superior

---
+++ ex17.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Exercicio17 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] A = new int[4][4];
+
+        System.out.println("Digite os elementos da matriz A 4x4:");
+        for (int i = 0; i < 4; i++)
+            for (int j = 0; j < 4; j++)
+                A[i][j] = sc.nextInt();
+
+        for (int i = 1; i < 4; i++)
+            for (int j = 0; j < i; j++)
+                A[i][j] = 0;
+
+        System.out.println("\nMatriz triangular superior:");
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++)
+                System.out.print(A[i][j] + "\t");
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
